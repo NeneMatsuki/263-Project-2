bd5c29d3b11c45f3028be373ed42a2a9fbd42d9b
nmat657 <80789350+nmat657@users.noreply.github.com>
2021-10-10 01:11:56 +1300

    created bootstrap distribution for the demand

--- bootstrap_demands.py
+++ bootstrap_demands.py
@@ -1,61 +1,71 @@
-from math import ceil
-from scipy import stats
-import numpy as np
 import csv
-import pandas as pd
 import random 
 from utilities import *
 import matplotlib.pyplot as plt
 import seaborn as sns
 import statistics
 import statsmodels.stats.weightstats as sms
+import os
 
-def get_random_demand_mt():
-    with open("mon_thurs_pool.csv", mode = 'r') as f:
+def get_random_demand(day):
+    # this function creates a dictionary of each store with a randomly sampled demand from the pool
+
+    # open and read the specific demand pool file
+    with open("demand_pool" + os.sep + f"{day}_pool.csv", mode = 'r') as f:
         f.readline()
         reader = csv.reader(f)
-        dict = {rows[0]:int(rows[random.randint(1,15)]) for rows in reader}
+        if (day == "sat"):
+            dict = {rows[0]:int(rows[random.randint(1,3)]) for rows in reader}   
+        else:
+            dict = {rows[0]:int(rows[random.randint(1,15)]) for rows in reader}
+    # return the dictionary  
     return dict
 
-def mt_get_rand_cost():
-    with open("m_t.optimal.routes.txt") as f:
-        routes = [tuple(line.strip().split(",")) for line in f.readlines()]
-    demands_mt = get_random_demand_mt()
-    return(sum(get_cost_of_route(route,demands_mt) for route in routes))
+def get_rand_cost(day):
+    # this function gets the total cost of the optimal routes
 
-def get_cost_of_route(route, demand):
-     
-    number_of_pallets = sum(demand.get(store,0) for store in route)  
-    travel_duration = sum(travel_durations[store1][store2] for store1, store2 in zip(route, route[1:]))
-    minutes = ceil((travel_duration + number_of_pallets * TIME_PER_PALLET)/60)
+    # open the optimal route file and get the cost
+    with open(f"{day}.optimal.routes.txt") as f:
+        routes = [tuple(line.strip().split(",")) for line in f.readlines()]
+    demands = get_random_demand(day)
+    return(sum(get_cost_of_bootroute(route,demands) for route in routes))
+
+def get_cost_of_bootroute(route, demand):
+    # this function returns the cost of a single route subject to the specified demand
+    number_of_pallets = sum(demand.get(store,0) for store in route)                                        # get the number of pallets
+    travel_duration = sum(travel_durations[store1][store2] for store1, store2 in zip(route, route[1:]))    # get the travel duration
+    minutes = ceil((travel_duration + number_of_pallets * TIME_PER_PALLET)/60)                             # find the minutes it takes
     extrahrs = 0
+    # if minutes over than four hours, apply the extra cost
     if (minutes) > 240:
         extrahrs = minutes-240
-    return ( minutes * (COST_PER_HOUR/60) + extrahrs * (275/60))
+    return (minutes * (COST_PER_HOUR/60) + extrahrs * (275/60))
 
-
-    
-def main():
-    costs = np.zeros(1000)
+def plot_bench(day):
+    # this plots the bootstrap distribution of the plots and prints the mean and 95% bootstrap interval
+    # get 1000 random costs
+    costs = [None] * 1000
     for i in range (1000):
-        costs[i] = mt_get_rand_cost()
-    # print(costs)
-    
+        costs[i] = get_rand_cost(day)
+    costs.sort()    # sort the cost
+
     print("////////////////////////////////////////////////////////////////////////////")
-    print('The mean cost for Monday to Thurday is', statistics.mean(costs))
-    print('The 95%"" confidence interval is', sms.DescrStatsW(costs).tconfint_mean(alpha = 0.05))
+    print('The mean cost for '+ day +' is', statistics.mean(costs))
+    print('The 95%" bootstrap confidence interval is', costs[25], ",", costs[975])
     print("///////////////////////////////////////////////////////////////////////////")
 
+    # plot the data
     sns.displot(data = costs, binwidth = 200)
+
     plt.ylabel("frequency")
     plt.xlabel("cost of route (NZD)")
-    plt.title("Bootstrap distribution for cost of optimal route for Monday to Thurday(demands)")
+    plt.title("Bootstrap distribution for cost of optimal "+ day + " route for "+day+" (demands)")
     plt.show()
 
-
-
-
-    
+def main():
+    plot_bench("m_t")
+    plot_bench("fri")
+    plot_bench("sat")
 
 
 if __name__ == "__main__":
