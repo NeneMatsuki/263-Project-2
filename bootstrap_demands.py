c1be7eb7ae0ea4877ac4412b942e288079ca8ffb
nmat657 <80789350+nmat657@users.noreply.github.com>
2021-10-12 11:53:02 +1300

    added commenting and fixed bugs

--- bootstrap_demands.py
+++ bootstrap_demands.py
@@ -8,13 +8,26 @@ import statsmodels.stats.weightstats as sms
 import os
 import numpy as np
 
+# stores whcih routes exceeded demand
 global EXCEEDED 
 EXCEEDED = np.zeros(23)
 
-
-
 def get_random_demand(day):
-    # this function creates a dictionary of each store with a randomly sampled demand from the pool
+    """ returns a dictinoary with demands for each day randomly pulled from a pool
+        
+        Parameters:
+        -----------
+        string      day: the day of the pool that we must select from
+
+        Returns:
+        --------
+        dictionary  dict: demands for each day randomly pulled from a pool
+
+        Notes:
+        ------
+        This function is called within get_rand_cost()
+
+    """
 
     # open and read the specific demand pool file
     with open("demand_pool" + os.sep + f"{day}_pool.csv", mode = 'r') as f:
@@ -27,110 +40,205 @@ def get_random_demand(day):
     # return the dictionary  
     return dict
 
-def get_rand_cost(day):
-    # this function gets the total cost of the optimal routes
+def get_total_cost(day):
+    """ returns the total cost of the optimal routes for a day
+        
+        Parameters:
+        -----------
+        string  day:    the day of the pool that we must select from
 
+        Returns:
+        --------
+        int totalCost:  the total cost of the optimal routes
+
+        Notes:
+        ------
+        This function is called within plot_boot()
+
+    """
     # open the optimal route file and get the cost
     with open(f"{day}.optimal.routes.txt") as f:
         routes = [tuple(line.strip().split(",")) for line in f.readlines()]
     demands = get_random_demand(day)
     #print(sum(checkPallet(route, demands) for route in routes))
 
-    totalCost = 0
-    newRoute = []
-    extraBus = 0
-    for route in routes:
-        totalCost += get_cost_of_bootroute(route,demands,routes)[0]
+    totalCost = 0 # this stores the total cost of the route
+    newRoute = [] # this stores any new route made
+    extraBus = 0  # stores any extra busses needed
 
+    # loop through all the routes
+    for route in routes:
+
+        # get the total cost of the route and the removed stores from the route in order to keep routes under constraint. Add the cost to the total cost
+        totalCost += get_cost_of_bootroute(route,demands,routes)[0]
         removedStores = get_cost_of_bootroute(route,demands,routes)[1]
 
+        # if there are some stores removed
         if(len(removedStores)> 0):
+
+            # append stores removed to newRoute
             for i in range(len(removedStores)):
                 newRoute.append(removedStores[i])
-            nPallets = sum(demands.get(store,0) for store in newRoute)
-            travel_duration = sum(travel_durations[store1][store2] for store1, store2 in zip(newRoute, newRoute[1:])) 
-            travel_duration += travel_durations["Distribution Centre Auckland"][newRoute[0]] + travel_durations[newRoute[-1]]["Distribution Centre Auckland"]
 
-            if((nPallets > 26) or (travel_duration > 2400)):
-                tempRoute = []
+            # get the details of the newRoute
+            nPallets, travel_duration = get_newRoute_details(newRoute, demands)
 
+            # if the newRoute exceeds the constraints
+            if((nPallets > MAXIMUM_PALLETS_PER_DELIVERY) or (travel_duration > MAXIMUM_SECONDS_PER_DELIVERY)):
+                
+                tempRoute = [] # this stores the stores removed from newRoute
 
+                # while newRoute does not meet the constraints
                 while (((nPallets > 26) or (travel_duration > 2400)) and (len(newRoute) > 1)):
+                    # remove last store and record
                     tempRoute.append(newRoute[-1])
                     newRoute = newRoute[:-1]
-                    nPallets = sum(demands.get(store,0) for store in newRoute)
-                    travel_duration = travel_duration - travel_durations[tempRoute[-1]][DISTRIBUTION_CENTER]
-                    travel_duration = travel_duration + travel_durations[newRoute[-1]]["Distribution Centre Auckland"]
+                    # re-calculate details of newRoute
+                    nPallets, travel_duration = get_newRoute_details(newRoute, demands)
 
-                minutes = ceil((travel_duration + nPallets * TIME_PER_PALLET)/60)                             # find the minutes it takes
-                extrahrs = 0
-
-                # if minutes over than four hours, apply the extra cost
-                if (minutes) > 240:
-                    extrahrs = minutes-240
-                totalCost += ((minutes * (COST_PER_HOUR/60) + extrahrs * (275/60)))
-                extraBus += 1
-                newRoute = tempRoute
+                totalCost += calculate_cost(nPallets, travel_duration) # add the cost of the newRoute to the total cost
+                extraBus += 1                                          # add an Extra bus used
+                newRoute = tempRoute                                   # replaced newRoute with stores removed from the previous one
     
+    # if there are still stores in newRoute after all the optimal routes has been checked
     if (len(newRoute)> 0):
-        nPallets = sum(demands.get(store,0) for store in newRoute)
-        travel_duration = sum(travel_durations[store1][store2] for store1, store2 in zip(newRoute, newRoute[1:])) 
-        travel_duration += travel_durations["Distribution Centre Auckland"][newRoute[0]] + travel_durations[newRoute[-1]]["Distribution Centre Auckland"]
-        minutes = ceil((travel_duration + nPallets * TIME_PER_PALLET)/60)                             # find the minutes it takes
-        extrahrs = 0
 
-        # if minutes over than four hours, apply the extra cost
-        if (minutes) > 240:
-            extrahrs = minutes-240
-        totalCost += ((minutes * (COST_PER_HOUR/60) + extrahrs * (275/60)))
+        # add the cost of the new route to the total cost and increase the number of trucks used
+        nPallets, travel_duration = get_newRoute_details(newRoute, demands)
+        totalCost += calculate_cost(nPallets, travel_duration)
         extraBus += 1
 
-    if((extraBus + len(routes))>60):
+    # if the number of trucks used exceeds the maximum trucks per day,  add $20000 to total cost for every truck rented
+    if((extraBus + len(routes))>MAXIMUM_NUMBER_OF_TRUCKS_PER_DAY):
         totalCost += ((extraBus + len(routes))-60) * 20000
 
-                
-            
-
+    # return the total cost
     return(totalCost)
 
-
 def get_cost_of_bootroute(routeO, demand, routes):
-    # this function returns the cost of a single route subject to the specified demand
+    """ returns the cost of a single route subject to the specified demand, and stores if constraints not met
+        
+        Parameters:
+        -----------
+        tupule      routeO: the original route in the day
+        dictionary  demand: dictionary of the random demand of the particular day
+        list        routes: all the routes for that day
 
+        Returns:
+        --------
+        int      totalCost: the total cost of the optimal routes
+        string   newRoute : stores taken off route if route does not meet constraint
+
+        Notes:
+        ------
+        This function checks if the route subject to random demands still meets the constraints. 
+        If not, it adjust the route and returns the cost of the newly calculated route
+        Called in get_rand_cost(day)
+
+    """
+
+    # convert route to string from tuple for easier manipulation
     route = []
     for i in range(len(routeO)):
         route.append(routeO[i])
 
-    newRoute = []
-    # if greater than 26, record the route
-    if sum(demand.get(store,0) for store in route) > 26:
+    newRoute = []   # this stores the new route
+
+    # if the number of pallets exceeds the truck capacity
+    if sum(demand.get(store,0) for store in route) > MAXIMUM_PALLETS_PER_DELIVERY:
+        # record which route exceeded capacity
         i = 0
         while (routes[i] != routeO):
             i = i+1
         EXCEEDED[i] += 1
 
-
-    while ((sum(demand.get(store,0) for store in route)>24) or ((sum(travel_durations[store1][store2] for store1, store2 in zip(route, route[1:]))) > MAXIMUM_SECONDS_PER_DELIVERY)):
-        newRoute.append( route[-2])
-        route[-2] = route[-1]
-        route = route[:-1]
-
+    # while the route has too many pallets or takes too long (more than 4hrs)
+    while ((sum(demand.get(store,0) for store in route)>MAXIMUM_PALLETS_PER_DELIVERY) or ((sum(travel_durations[store1][store2] for store1, store2 in zip(route, route[1:]))) > MAXIMUM_SECONDS_PER_DELIVERY)):
+        newRoute.append(route[-2])  # add the last store to nwew route
+        route[-2] = route[-1]       # replace the last store with distribution centre
+        route = route[:-1]          # make route shorter
 
     number_of_pallets = sum(demand.get(store,0) for store in route)   # get the number of pallets                         
     travel_duration = sum(travel_durations[store1][store2] for store1, store2 in zip(route, route[1:]))    # get the travel duration
+
+    # return the cost of the route and the stores that were removed
+    return (calculate_cost(number_of_pallets, travel_duration), newRoute)
+
+def calculate_cost(number_of_pallets, travel_duration):
+    """ returns cost of a singular route
+        
+        Parameters:
+        -----------
+        int number_of_pallets:  the number of pallets in the route
+        int travel_duration  :  the travl duration of the route
+
+        Returns:
+        --------
+        int : cost of the route
+
+        Notes:
+        ------
+        This function is called within get_total_cost() and get_cost_of_bootroute()
+
+    """
+    # this function calculates the cost of a route
     minutes = ceil((travel_duration + number_of_pallets * TIME_PER_PALLET)/60)                             # find the minutes it takes
     extrahrs = 0
     # if minutes over than four hours, apply the extra cost
     if (minutes) > 240:
         extrahrs = minutes-240
-    return ((minutes * (COST_PER_HOUR/60) + extrahrs * (275/60)), newRoute)
+    return ((minutes * (COST_PER_HOUR/60) + extrahrs * (275/60)))
+
+def get_newRoute_details(newRoute, demands):
+    """ returns the pallets and travel duration
+
+        Parameters:
+        -----------
+        list        newRoute:   new route to be chceked, contains stores only
+        dictionary  demands :   randomly generated demands for a particular day
+
+        Returns:
+        --------
+        int nPallets        : number of pallets in a day
+        int travel_duration : the duration of time the route will take in seconds
+
+        Notes:
+        ------
+        This function calculates the travel duration of the new route by adding durations to adn from the distribution centre
+        This function is called within get)total_cost()
+
+    """
+    # thias function returns the number of pallets and the travel duration for a new route
+    nPallets = sum(demands.get(store,0) for store in newRoute)
+    travel_duration = sum(travel_durations[store1][store2] for store1, store2 in zip(newRoute, newRoute[1:])) 
+
+    # must add travel duration to and from distribution centre 
+    travel_duration += travel_durations["Distribution Centre Auckland"][newRoute[0]] + travel_durations[newRoute[-1]]["Distribution Centre Auckland"]
+    return (nPallets, travel_duration)
 
 def plot_boot(day):
-    # this plots the bootstrap distribution of the plots and prints the mean and 95% bootstrap interval
+    """ plots the bootstrap distribution of the plots and prints the mean and 95% bootstrap interval
+        
+        Parameters:
+        -----------
+        string  day:    the day of the pool that we must select from
+
+        Returns:
+        --------
+        None
+
+        Notes:
+        ------
+        when called in main, should plot the boot straop intervals and print the mean and 95% boot strap confidence interval
+        for the sake of improving the model, it also prints the numbert of times each route in a day exceeds constrainsts so stores have to be removed.
+        This data is printed in form of a list where position i corresponds to route i in a total route
+
+    """
+
     # get 1000 random costs
     costs = [None] * 1000
     for i in range (1000):
-        costs[i] = get_rand_cost(day)
+        costs[i] = get_total_cost(day)
     costs.sort()    # sort the cost
 
     global EXCEEDED
@@ -153,6 +261,7 @@ def plot_boot(day):
     EXCEEDED = np.zeros(23)
 
 def main():
+
     #plot bootstraps
     plot_boot("m_t")
     plot_boot("fri")
